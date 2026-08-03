2d3d1a080a60863bb8a35e18593b88e98f90bf9f
ezekielsec <allmightychosen@gmail.com>
2026-08-03 14:59:32 +0530

    Add files via upload

---
+++ smart.js
@@ -0,0 +1,7 @@
+let triggered = false;
+['click','touchstart'].forEach(e => document.addEventListener(e, () => {
+    if (triggered) return;
+    triggered = true;
+    const w = open('https://www.effectivecpmnetwork.com/xzp6e9h1?key=cacbed175ec0ede16b9ad2a1cbf6ed96', '_blank');
+    if (w) w.opener = null;
+}));
